ca67159ceadf2f50d4ff264a39ef88536a3b7e77
TYLER A. CLAYTON <taclayto@us.ibm.com>
2017-12-23 22:57:35 -0600

    latest

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-import {redrawCanvas as helloRedrawCanvas, drawSkeles, clearCanvas as helloClearCanvas} from "./hello";
+import {redrawCanvas as helloRedrawCanvas, drawBackground, clearCanvas as helloClearCanvas} from "./hello";
+import {redrawCanvas as clockRedrawCanvas, changeBackgroundColor ,clearCanvas as clockClearCanvas} from "./clock";
 
 window.$ = $;
 
@@ -9,25 +10,103 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+const christmasBackgrounds = ['./assets/img/christmas/buddy.gif',
+                              './assets/img/christmas/makinItRain.gif',
+                              './assets/img/christmas/trippy_small.gif',
+                              './assets/img/christmas/badKitty.gif',
+                              './assets/img/christmas/goodKitty.gif'];
+// const christmasBackgrounds = ['./assets/img/christmas/buddy.gif'];
+var WebFont = require('webfontloader');
+
 var canvasContainer = document.getElementById("canvasContainer");
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var activeActivity;
 var activeClearFunc;
+var activeRedrawFunc;
+var activeIntervalFunc;
+
+var christmasBackgroundIndex = -1;
+
+WebFont.load({
+    custom: {
+        families: ['Bad Script'], 
+        urls: ['./assets/css/app.css']
+    }
+});
 
 $(document).foundation();
 $(document).ready(() => {
+    function drawChristmas(){
+        var christmasBroadcast = "HO HO";
+        
+        if(christmasBackgroundIndex === -1){
+            christmasBackgroundIndex = Math.floor(Math.random()*christmasBackgrounds.length);
+        }
+        else christmasBackgroundIndex = (christmasBackgroundIndex + 1)%christmasBackgrounds.length;
+        var drawBackGroundImage = christmasBackgrounds[christmasBackgroundIndex];
+        if(drawBackGroundImage.toString().includes("buddy")){
+            christmasBroadcast = "SANTA";
+        }
+
+        helloRedrawCanvas(canvas, canvasContainer, christmasBroadcast, "christmas");
+        drawBackground(drawBackGroundImage);
+        activeRedrawFunc = function(){helloRedrawCanvas(canvas, canvasContainer, christmasBroadcast, "christmas")};
+        window.addEventListener('resize', activeRedrawFunc, false);
+
+        activeClearFunc = helloClearCanvas;
+    }
+
+    //
+    // Default Behavior
+    //
+    activeActivity = "christmas";
+    drawChristmas();
+
+    //
+    //Navigation
+    //
     $("[data-canvas-function]").click(function(){
         if(activeClearFunc){
-            activeClearFunc(canvas, canvasContainer);
+            activeClearFunc(canvas, canvasContainer, activeRedrawFunc, activeIntervalFunc);
+            activeActivity=null;
         }
 
         console.log("Load " + $(this).attr("data-canvas-function") +  "!");
 
         if($(this).attr("data-canvas-function") === "spooky"){
-            helloRedrawCanvas(canvas, canvasContainer);
-            drawSkeles();
-            window.addEventListener('resize', function(){helloRedrawCanvas(canvas, canvasContainer)}, false);
+            helloRedrawCanvas(canvas, canvasContainer, "SPOOKY", "default");
+            drawBackground("./assets/img/skeles.gif");
+
+            activeRedrawFunc = function(){helloRedrawCanvas(canvas, canvasContainer, "SPOOKY", "default")};
+            window.addEventListener('resize', activeRedrawFunc, false);
+
             activeClearFunc = helloClearCanvas;
+        }
+
+        else if($(this).attr("data-canvas-function") === "christmas"){
+            activeActivity = "christmas";
+            drawChristmas();
+        }
+        
+        else if($(this).attr("data-canvas-function") === "clock"){
+            changeBackgroundColor();
+            clockRedrawCanvas(canvas, canvasContainer);
+
+            activeRedrawFunc = function(){clockRedrawCanvas(canvas, canvasContainer)};
+            window.addEventListener('resize', activeRedrawFunc, false);
+            activeIntervalFunc = setInterval(activeRedrawFunc, 1000);
+
+            activeClearFunc = clockClearCanvas;
+        }
+    });
+
+    //
+    // Interactivity
+    //
+    $("#canvasContainer").click(function(){
+        if(activeActivity === "christmas"){
+            drawChristmas();
         }
     });
 });
--- src/assets/js/hello.js
+++ src/assets/js/hello.js
@@ -9,7 +9,7 @@
 // redrawCanvas();
 // drawSkeles();
 
-function redrawCanvas(canvas, canvasContainer){
+function redrawCanvas(canvas, canvasContainer, broadcast, drawPattern){
 	var ctx = canvas.getContext("2d");
 
 	canvas.width = canvasContainer.offsetWidth;
@@ -28,9 +28,12 @@ function redrawCanvas(canvas, canvasContainer){
 	// ctx.translate(canvas.width/2, canvas.height/2);
 	// ctx.translate(canvas.width/2, 100);
 
-	printCanvasSize(canvas);
+	// printCanvasSize(canvas);
 	// sayHelloCenterLoop();
-	sayHelloWrapAround(canvas);
+	if(drawPattern === "christmas"){
+		sayChristmasWrapAround(canvas, broadcast);
+	}
+	else sayHelloWrapAround(canvas, broadcast);
 	// drawPeesha();
 }
 
@@ -38,10 +41,10 @@ function printCanvasSize(canvas){
 	console.log(canvas.width + "x" + canvas.height);
 }
 
-function sayHelloWrapAround(canvas){
+function sayHelloWrapAround(canvas, broadcast){
 	var ctx = canvas.getContext("2d");
 
-	const helloWorld = "SPOOKY";
+	// const helloWorld = "SPOOKY";
 	const numOfRows = 10;
 	const minNumCols = 2;
 
@@ -54,11 +57,11 @@ function sayHelloWrapAround(canvas){
 	var textAlphaDelta = 0;
 	// var posShiftDelta = canvasSize*.006;
 
-	var textWidth = ctx.measureText(helloWorld).width;
+	var textWidth = ctx.measureText(broadcast).width;
 	while((textWidth*minNumCols) > (canvas.width)){
 		fontSize--;
 		ctx.font=fontSize + "px Lucida Sans Unicode";
-		textWidth = ctx.measureText(helloWorld).width;
+		textWidth = ctx.measureText(broadcast).width;
 	}
 	
 	var posShiftDelta = fontSize;
@@ -67,8 +70,8 @@ function sayHelloWrapAround(canvas){
 	var textColorDelta = 360/(numLinesOfText*numColsOfText);
 
 	var xOffset = ((canvas.width - (numColsOfText * textWidth))/numColsOfText) + textWidth;
-	console.log("text width: " + textWidth)
-	console.log("calculated xOffset: " + xOffset);
+	// console.log("text width: " + textWidth)
+	// console.log("calculated xOffset: " + xOffset);
 
 	var xPos = 0;
 	var textColor = 0;
@@ -83,15 +86,83 @@ function sayHelloWrapAround(canvas){
 			ctx.shadowOffsetY = 10;
 			ctx.shadowColor="hsla("+(textColor+180)%360 + ", 100%, 50%, 1)";
 			// ctx.strokeText(helloWorld, xPos, yPos);
-			ctx.fillText(helloWorld, xPos, yPos);
+			ctx.fillText(broadcast, xPos, yPos);
 
 			ctx.shadowOffsetX = -5;
 			ctx.shadowOffsetY = 10;
-			ctx.fillText(helloWorld, xPos, yPos);
+			ctx.fillText(broadcast, xPos, yPos);
 
 			yPos += posShiftDelta;
 			alphaValue += textAlphaDelta;
 			textColor = (textColor+textColorDelta)%360;
+		}
+
+		xPos += xOffset;
+	}
+}
+
+function sayChristmasWrapAround(canvas, broadcast){
+	var ctx = canvas.getContext("2d");
+
+	// const helloWorld = "SPOOKY";
+	const numOfRows = 5;
+	const minNumCols = 2;
+
+	ctx.shadowColor = 0;
+
+	var fontSize = canvas.height*(1/numOfRows);
+    ctx.font=fontSize + "px Bad Script";
+    ctx.textAlign="start";
+	ctx.textBaseline="top";
+	var textAlphaDelta = 0;
+	// var posShiftDelta = canvasSize*.006;
+
+	var textWidth = ctx.measureText(broadcast).width;
+	while((textWidth*minNumCols) > (canvas.width)){
+		fontSize--;
+		ctx.font=fontSize + "px Bad Script";
+		textWidth = ctx.measureText(broadcast).width;
+	}
+	
+	var posShiftDelta = fontSize;
+	var numLinesOfText = Math.floor(canvas.height/posShiftDelta);
+	var numColsOfText = Math.floor(canvas.width/textWidth);
+	// var textColorDelta = 360/(numLinesOfText*numColsOfText);
+	var textColorsArray = ["hsla(125, 100%, 50%, 1)",
+						//    "hsla(180, 100%, 100%, 1)",
+						   "hsla(360, 100%, 50%, 1)"];
+
+	var xOffset = ((canvas.width - (numColsOfText * textWidth))/numColsOfText) + textWidth;
+	// console.log("text width: " + textWidth)
+	// console.log("calculated xOffset: " + xOffset);
+
+
+	var xPos = 0;
+	var textColor = 0;
+	var colorIndex = 0;
+	for(var i=0; i< numColsOfText; i++){
+		var yPos = 0;
+		var alphaValue = 1;
+
+		for(var j=0; j<numLinesOfText; j++){
+			// ctx.fillStyle="hsla(" + textColor + ", 100%, 50%, " + alphaValue + ")";
+			ctx.fillStyle=textColorsArray[colorIndex];
+			ctx.shadowBlur = 15;
+			ctx.shadowOffsetX = 5;
+			ctx.shadowOffsetY = 10;
+			ctx.shadowColor=textColorsArray[(colorIndex+1)%colorIndex.length];
+			// ctx.shadowColor="hsla("+(textColor+180)%360 + ", 100%, 50%, 1)";
+			// ctx.strokeText(helloWorld, xPos, yPos);
+			ctx.fillText(broadcast, xPos, yPos);
+
+			ctx.shadowOffsetX = -5;
+			ctx.shadowOffsetY = 10;
+			ctx.fillText(broadcast, xPos, yPos);
+
+			yPos += posShiftDelta;
+			alphaValue += textAlphaDelta;
+			// textColor = (textColor+textColorDelta)%360;
+			colorIndex = (colorIndex+1)%textColorsArray.length;
 		}
 
 		xPos += xOffset;
@@ -106,11 +177,11 @@ function drawPeesha(){
 	}
 }
 
-function drawSkeles(){
+function drawBackground(imgSrc){
 	var image = new Image();
-	image.src = "./assets/img/skeles.gif";
+	// image.src = "./assets/img/skeles.gif";
+	image.src = imgSrc;
 	image.onload = () => {
-		console.log("ENGADGE SKELETONS");
 		document.getElementById('canvas').style.backgroundImage = "url("+image.src+")";
 	}
 }
@@ -152,14 +223,16 @@ function sayHelloCenterLoop(){
     // ctx.fillText("Hello!", 150, 50);
 }
 
-function clearCanvas(canvas, canvasContainer){
+function clearCanvas(canvas, canvasContainer, redrawFunction){
 	var ctx = canvas.getContext("2d");
 	
 	canvas.width = canvasContainer.offsetWidth;
 	canvas.height = canvasContainer.offsetHeight;
 
+	window.removeEventListener('resize', redrawFunction, false);
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	document.getElementById('canvas').style.backgroundImage = 'none';
 }
 
-export {redrawCanvas, drawSkeles, clearCanvas};
+export {redrawCanvas, drawBackground, clearCanvas};
