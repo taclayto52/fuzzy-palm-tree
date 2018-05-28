de6ceefb505a064bbbcb83224e3f8a5a94773fdb
TYLER A. CLAYTON <taclayto@us.ibm.com>
2018-05-28 10:37:36 -0400

    improved search caching

--- src/assets/js/giphy.js
+++ src/assets/js/giphy.js
@@ -9,6 +9,8 @@ const apiKey = "Wojl1VNfBn652GfHqzYOpQ8LBrWQQe5d";
 const gifSizes= ["original", "downsized", "downsized_large", "downsized_medium", "downsized_small"];
 
 var currentImageIndex = 0;
+let currentImageSearchResults;
+let currentSearchQuery;
 
 var apiTranslateParams = {
     api_key: apiKey,
@@ -24,6 +26,32 @@ var apiSearchParams = {
     lang: "en"
 }
 
+function _resolveGiphyResultsToUrlBasedOnCurrentIndex(){
+    var gifUrl;
+    
+    if(!currentImageSearchResults || currentImageSearchResults.pagination.count === 0){
+        //TODO get static not found gif
+        gifUrl = "./assets/img/not_found.gif";
+    }else{
+        if(currentImageIndex >= currentImageSearchResults.pagination.count) currentImageIndex = 0;
+
+        var gifSizesIndex = 0;
+        var gifData = currentImageSearchResults.data[currentImageIndex++].images[gifSizes[gifSizesIndex]];
+        while((gifData.height > canvas.height) || (gifData.width > canvas.width)){
+            gifData = currentImageSearchResults.data[currentImageIndex].images[gifSizes[gifSizesIndex++]];
+            if(gifSizesIndex === gifSizes.length){
+                break;
+            }
+        }
+        gifUrl = gifData.url;
+    }
+    
+    currentImageIndex = currentImageIndex%(currentImageSearchResults.pagination.count);
+
+    return(gifUrl);
+}
+
+// *** Exported functions ****
 function searchGiphy(searchTerm, canvas){
     return new Promise((resolve, reject) => {
         var urlObject = {
@@ -34,33 +62,22 @@ function searchGiphy(searchTerm, canvas){
         }
 
         urlObject.query.q = searchTerm;
-        $.getJSON(url.format(urlObject), function(respData){
-            var gifUrl;
+        let searchQueryChanged = currentSearchQuery !== searchTerm;
+        if(searchQueryChanged || !currentImageSearchResults){
+            currentSearchQuery = searchTerm;
 
-            if(respData.pagination.count === 0){
-                //TODO get static not found gif
-                gifUrl = "./assets/img/not_found.gif";
-            }else{
-                if(currentImageIndex >= respData.pagination.count) currentImageIndex = 0;
-
-                var gifSizesIndex = 0;
-                var gifData = respData.data[currentImageIndex++].images[gifSizes[gifSizesIndex]];
-                while((gifData.height > canvas.height) || (gifData.width > canvas.width)){
-                    gifData = respData.data[currentImageIndex].images[gifSizes[gifSizesIndex++]];
-                    if(gifSizesIndex === gifSizes.length){
-                        break;
-                    }
-                }
-                gifUrl = gifData.url;
-            }
-            
-            currentImageIndex = currentImageIndex%(respData.pagination.count);
-
-            resolve(gifUrl);
-        })
-        .fail(function(){
-            reject();
-        });
+            $.getJSON(url.format(urlObject), function(respData){
+                currentImageSearchResults = respData;
+            })
+            .fail(function(){
+                reject();
+            })
+            .always(function(){
+                resolve(_resolveGiphyResultsToUrlBasedOnCurrentIndex());
+            });
+        } else {
+            resolve(_resolveGiphyResultsToUrlBasedOnCurrentIndex());
+        }
     });
 }
 
